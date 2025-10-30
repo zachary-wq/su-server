a8bc496ef56706f54cd6bfe1c904d7cad6920b9c
Zachary <msmsruby@gmail.com>
2025-10-30 20:06:03 +0800

    Add Chinese and English localization for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,27 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
+  before_action :set_locale
+
   rescue_from ActionController::InvalidAuthenticityToken do |exception|
     sign_out_user # Example method that will destroy the user cookies
   end
-  
+
+  private
+    def set_locale
+      I18n.locale = extract_locale_from_params || I18n.default_locale
+    end
+
+    def extract_locale_from_params
+      locale = params[:locale]
+      return unless locale.present?
+
+      locale.to_sym if I18n.available_locales.map(&:to_s).include?(locale)
+    end
+
+    def default_url_options
+      return super unless I18n.locale
+
+      super.merge(locale: (I18n.locale unless I18n.locale == I18n.default_locale)).compact
+    end
 end
